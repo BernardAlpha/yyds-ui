6bcdb80144414015588ca7c2ffab133a380daa76
BernardAlpha <529341916@qq.com>
2023-02-23 19:02:33 +0800

    Remove useless comments

--- packages/yyds-ui/index.ts
+++ packages/yyds-ui/index.ts
@@ -1,12 +1,9 @@
-// import { WIcon,WButton } from "@yyds-ui/components";
 import * as components from "@yyds-ui/components";
 import type { App } from "vue";
 
-// const components = [WIcon,WButton];
-
 const install = (app: App) => {
   // 每个组件在写的时候都提供了install方法
-
+  
   // 有的是组件，有的可能是指令 xxx.install = () => { app.directive() }
   // components.forEach((component) => app.use(component));
 
@@ -15,10 +12,8 @@ const install = (app: App) => {
   });
 };
 
-// app.use(WPlus)
 export default {
   install,
 };
 
-// import { WIcon } from 'yyds-ui
 export * from "@yyds-ui/components";
